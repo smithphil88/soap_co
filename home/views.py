29398c42f861adcb64e6d1dabaf56e3741155359
smithphil88 <smithphil868@gmail.com>
2025-03-12 21:31:08 +0000

    added mission page, urls and views

--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def mission(request):
+    """ A view to return the mission page """
+
+    return render(request, 'home/mission.html')
